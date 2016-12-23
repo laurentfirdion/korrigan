fdb49ca2782689eded42ec2631d04e88bd0c89db
Firdion Laurent <lfirdion@gmail.com>
2016-12-23 23:17:09 +0100

    [feat] page de base html/php

--- structure.php
+++ structure.php
@@ -48,21 +48,41 @@
         
 
         <h2><?php the_field('titre_2'); ?></h2>
+            
+              <img src="<?php the_field('image_2'); ?>" />
    
             <?php the_field('texte_2'); ?>
     </div>
     
-        <div id="partie3" class="mainmetiertext">
+    <div id="partie3" class="mainmetiertext">
         
        
      
 
         <h2><?php the_field('titre_3'); ?></h2>
+        
+          <img src="<?php the_field('image_3'); ?>" />
+        
              <?php the_field('texte_3'); ?>
     
     </div>
     
+    <?php $value = get_field('titre_4');
+    if(isset($value[0])) { ?>   
+    
+    <div id="partie4" class="mainmetiertext">
 
+        <h2><?php the_field('titre_4'); ?></h2>
+        
+           <img src="<?php the_field('image_4'); ?>" />
+        
+        
+             <?php the_field('texte_4'); ?>
+    
+    </div>
+    
+    
+    <?php } ?>   
     
      
     
@@ -72,10 +92,12 @@
        
    <ul class="nav">
        
-        <li><a href="#partie1"><?php the_field('titre_1'); ?></a></li>
-         <li><a href="#partie2"><?php the_field('titre_2'); ?></a></li>
-        <li><a href="#partie3"><?php the_field('titre_3'); ?></a></li>
-     
+        <li><a href="#partie1"><?php the_field('titre_1'); ?> <i class="fa fa-angle-right"><span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></i></a></li>
+         <li><a href="#partie2"><?php the_field('titre_2'); ?><i class="fa fa-angle-right"><span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></i></a></li>
+        <li><a href="#partie3"><?php the_field('titre_3'); ?><i class="fa fa-angle-right"><span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></i></a></li>
+        <?php if(isset($value[0])) { ?>   
+       <li><a href="#partie4"><?php the_field('titre_4'); ?><i class="fa fa-angle-right"><span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></i></a></li>
+        <?php } ?>   
     </ul>
       
     </nav>
@@ -84,43 +106,6 @@
 </div>
 
 
-<script>
-jQuery(document).ready(function($) {
-$('body').scrollspy({ target: '#navbarside', offset:400 })
-
-$('#navbarside').affix({
-  offset: {
-    top: 50,
-    bottom: function () {
-      return (this.bottom = $('.footer').outerHeight(true))
-    }
-  }
-});
-
-var width = $('.aside').width();    
-$('#navbarside.affix').css({'width':width});
-
-
-var navOffset = $('.navbar-fixed-top').height();
-    
-
-$('#navbarside .nav li a').click(function(event) {
-   event.preventDefault();
-var target = $(this).attr('href');
-    $('html, body')
-       // on arrête toutes les animations en cours 
-       .stop()
-       /* on fait maintenant l'animation vers le haut (scrollTop) vers 
-        notre ancre target */
-       .animate({scrollTop: $(target).offset().top-30}, 1000 );
-});
-
-
-
-
-
-});
-</script>    
 <?php 
 
 get_footer(); ?>
