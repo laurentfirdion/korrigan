55a12c6dee70a274fa627f87c98a0b5352625b85
Firdion Laurent <lfirdion@gmail.com>
2017-01-10 23:14:57 +0100

    [feat] début page contact

--- contact.php
+++ contact.php
@@ -4,47 +4,71 @@
  get_header();
 	?>  
 
-  <div class="container" id="filariane"> 
-           <div class="retour hvr-bubble-float-left"><a href="javascript:history.go(-1)"><img src="<?php bloginfo('template_url'); ?>/img/retour2.png" alt="bouton retour"/></a></div><div class="ariane"><ol class="breadcrumb">
-  <li> <a href="<?php bloginfo('url'); ?>">ACCUEIL</a></li>
-  <li><?php the_title(); ?></li>
-  
-</ol>
-           </div>
-           <br class="stopfloat"/>
+<div class="container-fluid" id="filariane">
+    <div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="ariane">
+                <ol class="breadcrumb">
+                    <li> <a href="<?php bloginfo('url'); ?>">ACCUEIL</a></li>
+                    <li>
+                        <?php the_title();?>
+                    </li>
+                </ol>
+            </div>
+            <br class="stopfloat" />
+        </div>
     </div>
-<div class="container">
-<div class="col-xs-12 col-sm-8 col-md-8 textemain">
-   
-<h1><?php the_field('titre'); ?></h1>
-    
- <?php the_field('formulaire_texte'); ?>   
-
-  <div class="formulaire">
-   <?php the_field('formulaire'); ?> 
-  </div>
-    
-    
-    </div>  
-  <div class="col-xs-12 col-sm-4 col-md-4">
-      <div class="side">
-     <?php the_field('side'); ?>
-          <h5><img class="petit hvr-buzz" src="<?php bloginfo('template_url'); ?>/img/oldphone.png"/><?php the_field('fixe'); ?></h5>
-           <h5><img class="petit hvr-wobble-top" src="<?php bloginfo('template_url'); ?>/img/mobile.png"/> <?php the_field('mobile'); ?></h5>
-           <h5><img class="petit hvr-sink" src="<?php bloginfo('template_url'); ?>/img/fax.png"/> <?php the_field('fax'); ?></h5>
-          </div>
-</div>   
-    
- <div class="col-xs-12 col-sm-8 col-md-8 tarifmain">   
-  <div class="tarif">
-     <?php the_field('tarif'); ?> 
-    
-    
-   </div>
-   
+    <div class="background">
+        <h1><?php the_title();?></h1>
     </div>
- 
 </div>
-<?php 
 
+<div class="container"> 
+
+    
+<div class="col-xs-12 col-sm-3 col-md-3 aside hide_mobile">
+    <nav id="navbarside" class="affix-top">
+       
+
+      
+ <div class="actu-side">
+             <h3>ACTUALITÉ</h3>
+    
+<?php $args = array(
+	'posts_per_page' => 1,
+	'post__in'  => get_option( 'sticky_posts' ),
+	'ignore_sticky_posts' => 1
+);
+$query = new WP_Query( $args ); 
+
+?>
+
+<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+    
+    <div class="sideactu">
+    <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+        <p><?php the_excerpt(); ?></p>
+    </div>
+<?php endwhile; wp_reset_query();?>
+  </div>
+        
+    </nav>
+  
+    
+</div>    
+    
+    
+<div class="col-xs-12 col-sm-9 col-md-9 mainmetier">
+    
+<?php echo do_shortcode( '[ninja_form id=1]' ); ?>
+  
+  
+     
+</div>
+
+
+</div>
+
+
+    <?php 
 get_footer(); ?>
